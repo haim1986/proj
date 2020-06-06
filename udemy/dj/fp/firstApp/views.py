0d16792015835bf561c82abbe41a3ee1888fca27
haim fadida <haim.fadida@wdc.com>
2020-06-06 11:50:55 +0300

    delete course folder
    Signed-off-by: haim fadida <haim.fadida@wdc.com>

--- udemy/dj/fp/firstApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    my_dict = {'insert':"coming from firstApp/index"}
-    return render(request, 'firstApp/index.html', context=my_dict)
-
-    #return  HttpResponse("<h1>hello Haim</h1>")
