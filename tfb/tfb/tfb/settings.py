4f1ed58c921c87dbd50cb628165b395899a7a560
haim fadida <haim.fadida@wdc.com>
2020-06-17 23:29:21 +0300

    part 4
    Signed-off-by: haim fadida <haim.fadida@wdc.com>

--- tfb/tfb/tfb/settings.py
+++ tfb/tfb/tfb/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+MEDIA_URL = '/images/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
